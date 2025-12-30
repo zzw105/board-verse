de5e9dbb9d8c2aee51d494943c030057451af161
zzw <13611782678@163.com>
2025-12-30 17:20:34 +0800

    迁移类型

--- packages/common/src/schema/SpiritIslandRoomState.ts
+++ packages/common/src/schema/SpiritIslandRoomState.ts
@@ -1,6 +1,42 @@
-import { Range, Range1ToN } from "@board-verse/common";
 import { ArraySchema, MapSchema, Schema, type } from "@colyseus/schema";
 
+/** 地图key枚举 */
+export const MapKeyE = {
+  map_1_a: "map_1_a",
+} as const;
+/** 地图key类型 */
+export type MapKeyT = (typeof MapKeyE)[keyof typeof MapKeyE];
+
+/** 区域key枚举 */
+export const RegionKeyE = {
+  map_1_a_0: "map_1_a_0",
+  map_1_a_1: "map_1_a_1",
+  map_1_a_2: "map_1_a_2",
+  map_1_a_3: "map_1_a_3",
+  map_1_a_4: "map_1_a_4",
+  map_1_a_5: "map_1_a_5",
+  map_1_a_6: "map_1_a_6",
+  map_1_a_7: "map_1_a_7",
+  map_1_a_8: "map_1_a_8",
+} as const;
+/** 区域key类型 */
+export type RegionKeyT = (typeof RegionKeyE)[keyof typeof RegionKeyE];
+
+/** 区域类型枚举 */
+export const RegionTypeE = {
+  /** 海洋 */
+  ocean: "ocean",
+  /** 林地 */
+  forest: "forest",
+  /** 沙地 */
+  sand: "sand",
+  /** 山地 */
+  mountain: "mountain",
+  /** 湿地 */
+  wetland: "wetland",
+} as const;
+/** 区域类型类型 */
+export type RegionTypeT = (typeof RegionTypeE)[keyof typeof RegionTypeE];
 // export class Inventory extends Schema {
 //   @type("string") name = "";
 //   @type("number") quantity = 0;
@@ -13,27 +49,35 @@ export class PlayerSchema extends Schema {
 
   // @type([Inventory]) inventory = new ArraySchema<Inventory>();
 }
-export type RegionKeyT = `map_${Range1ToN<1>}_${"a"}_${Range<8>}`;
-/** 区域类型枚举 */
-export enum RegionItemTypeE {
-  /** 海洋 */
-  ocean = "ocean",
-  /** 林地 */
-  forest = "forest",
-  /** 沙地 */
-  sand = "sand",
-  /** 山地 */
-  mountain = "mountain",
-  /** 湿地 */
-  wetland = "wetland",
+
+export class PointS extends Schema {
+  /** 定位点x坐标 */
+  @type("number") x = 0;
+  /** 定位点y坐标 */
+  @type("number") y = 0;
 }
+
 export class RegionS extends Schema {
   /** 区域key */
   @type("string") key!: RegionKeyT;
   /** 区域编号 */
   @type("number") regionNumber = 0;
   /** 区域类型 */
-  @type(RegionItemTypeE) regionType!: RegionItemTypeE;
+  @type("string") regionType!: RegionTypeT;
+  /** 探索者数量 */
+  @type("number") explorerCount = 0;
+  /** 村庄数量 */
+  @type("number") villageCount = 0;
+  /** 城镇数量 */
+  @type("number") townCount = 0;
+  /** 达昂人数量 */
+  @type("number") daangCount = 0;
+  /** 污染物数量 */
+  @type("number") polluteCount = 0;
+  /** 相连区域Key列表 */
+  @type(["string"]) adjacentRegionKeys = new ArraySchema<RegionKeyT>();
+  /** 灵迹数量 */
+  // spiritCount: Record<string, number>;
 }
 
 /** 地图S */
@@ -47,8 +91,6 @@ export class MapS extends Schema {
   /** 地图区域信息 */
   @type({ map: RegionS }) regions = new MapSchema<RegionS, RegionKeyT>();
 }
-
-export type MapKeyT = `map_${Range1ToN<1>}_${"a"}`;
 
 export class SpiritIslandRoomS extends Schema {
   @type({ map: PlayerSchema }) players = new MapSchema<PlayerSchema>();
--- packages/server/src/rooms/initData.ts
+++ packages/server/src/rooms/initData.ts
@@ -1,10 +1,9 @@
-import { MapS, RegionItemTypeE, RegionS } from "../schema/SpiritIslandRoomState";
+import { MapS, RegionS } from "../schema/SpiritIslandRoomState";
 
 const initData = new MapS();
 
 const a = new RegionS();
 a.key = "map_1_a_1";
 a.regionNumber = 1;
-a.regionType = RegionItemTypeE.ocean;
-
+a.adjacentRegionKeys.push("map_1_a_0");
 export { initData };
